12204e37808565cd16cfedb0df034ca924d2084c
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-08-04 09:58:52 +0200

    Initial commit - Mirrored from my site, file: python_archives/course_1/project1_9-1.py

---
+++ python_archives/course_1/project1_9-1.py
@@ -0,0 +1,36 @@
+#Task1
+#xpos=int(input("Xpos:"))
+#ypos=int(input("Ypos:"))
+#ypos1=""
+#xpos1=""
+#if ypos > 0:
+#    ypos1="felső"
+#else:
+#    ypos1="alsó"
+#if xpos > 0:
+#    xpos1="jobb"
+#else:
+#    xpos1="bal"
+#print(f"Ez a koordináta a {ypos1} {xpos1} részben van.")
+#lnko
+#print(bin(69420))
+#isprime= True
+#number1=int(input("szám?:"))
+#for i in range(2, number1//2):
+#    if number1 % i == 0:
+#        isprime = False
+#print(isprime)
+#word="vacation"
+#for i in range(1,len(word)):
+#    print(word[i])
+#    tmp = []
+#    tmp += [i]
+#    i+=1
+#tmp = []
+#for i in range(1,len(word)):
+#    tmp += [i]
+#    print(str(tmp))
+#    i+=1
+Lista=[0,1,2,3,4,86,5,6]
+print(len(Lista))
+

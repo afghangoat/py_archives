365b4207e04e507da5bb79674e96caafeddd0850
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-08-04 09:58:05 +0200

    Initial commit - Mirrored from my site, file: python_archives/course_1/project1_1alapok.py

---
+++ python_archives/course_1/project1_1alapok.py
@@ -0,0 +1,21 @@
+x = 1
+print("hello world")
+print("hell-o world")
+#\n newline
+"""
+asd
+ssd
+"""
+print("a /n az \nsortörés!")
+x = input("mi a neved?")
+print(x)
+print("hello!")
+input("mi a kedvenc színed " + x + "?")
+print("az tényleg szép szín!\n")
+
+print("Zöld erdőben jártam,\nKék python-t láttam,\nEl akart szaladni,\nNem szabad bántani!\n\nMostanában programoztam,\nÉs egy hiba itt és ott van,\nHát egy ' volna ottan,\nAzonnal kijavítottam!\n")
+print("""          /\ 
+         /  \ 
+        /    \ 
+       /      \ 
+       ˙˙˙˙˙˙˙˙""")

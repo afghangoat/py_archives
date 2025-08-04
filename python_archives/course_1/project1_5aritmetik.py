fb8970fa73ee59b18fe4f411d5e0d4268050a1c8
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-08-04 09:58:30 +0200

    Initial commit - Mirrored from my site, file: python_archives/course_1/project1_5aritmetik.py

---
+++ python_archives/course_1/project1_5aritmetik.py
@@ -0,0 +1,52 @@
+#5+=4
+#1. Aritemetikai Operátorok
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(7%4)
+print(3**3)
+print(99//4)
+#2. Értékadó operátor
+num1=8
+print(num1)
+num1+=8
+print(num1)
+num1*=4
+print(num1)
+num1/=4
+print(num1)
+num1-=4
+print(num1)
+num1**=4
+print(num1)
+#3. Összehasonlító operátorok
+print(5==5)
+print(5==6)
+print(5<=6)
+print(5>=6)
+print(9*5==15*3)
+#4. Identitás Operátor
+x1=5
+x2=5
+x3=8
+print(x1 is x2)
+print(x1 is x3)
+#5. Tagsági Operátor
+z1="H"
+z2="h"
+z3="K"
+text="Hi"
+print(z1 in text)
+print("world" in text)
+#6. Logikai Operátor
+print(2 and 3)
+print(2 or 3)
+print(not True and False)
+print(6/9 or 4<20)
+email=input(print("gmail:"))
+if ("@gmail" in email):
+    print("A jelszó helyes")
+else:
+    print("Ez invalid gmail!")
+#HF: 3 szám és melyik a legnagyobb

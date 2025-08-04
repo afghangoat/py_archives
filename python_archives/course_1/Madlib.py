a7746849835742d67e230ee74e5d2b059c33a85d
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-08-04 09:57:14 +0200

    Initial commit - Mirrored from my site, file: python_archives/course_1/Madlib.py

---
+++ python_archives/course_1/Madlib.py
@@ -0,0 +1,38 @@
+igemult1=input("ige (T/1):")
+igemult2=input("Múlt idejű ige (cselekvés)(T/1):")
+igemult3=input("ige:")
+igemult4=input("ige (E/1):")
+targy1=input("Egy tárgy:")
+targy3=input("Egy tárgy:")
+targy2=input("Egy tárgy:")
+epulet=input("Épület:")
+szemely=input("Keresztnév:")
+szemely2=input("Még egy keresztnév:")
+print(f"""Ez vagyok én, {szemely}. Éppen {igemult2} a {targy1}emben és kitaláltam, hogy milyen jó ötlet lenne venni
+egy {targy2}ot a(z) {epulet}ben. De közben elfelejtettem, hogy még nem fejeztem be a {targy3}-am elkészítését ezért
+elkezdtem {igemult1}ezni. Eközben átjött {szemely2} és elkezdtett {igemult3}ni. Én erre elkzedtem {igemult4}ezni és
+vége lett a napnak de nem vettem semmit.""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
